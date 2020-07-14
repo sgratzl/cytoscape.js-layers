2020a9da2035e94c08f6ec5532eb76253a8b51dc
Samuel Gratzl <sam@sgratzl.com>
2020-07-14 15:51:19 +0200

    start with annotation example

---
+++ samples/annotations.ts
@@ -0,0 +1,16 @@
+namespace Annotations {
+  declare const cytoscape: typeof import('cytoscape');
+  declare const CytoscapeLayers: typeof import('../dist');
+
+  export const cy = cytoscape({
+    container: document.getElementById('app'),
+    elements: fetch('./grid-data.json').then((r) => r.json()),
+    layout: {
+      name: 'grid',
+    },
+  });
+
+  const layers = CytoscapeLayers.layers(cy);
+
+  console.log(layers);
+}
