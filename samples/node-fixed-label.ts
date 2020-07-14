7df317fb85d6e64f12080eb8fb1fb959d6c5b801
Samuel Gratzl <sam@sgratzl.com>
2020-07-14 22:23:24 +0200

    add example with fixed label size

---
+++ samples/node-fixed-label.ts
@@ -0,0 +1,34 @@
+namespace NodeFixedHTMLLabels {
+  declare const cytoscape: typeof import('cytoscape');
+  declare const CytoscapeLayers: typeof import('../dist');
+
+  const cy = cytoscape({
+    container: document.getElementById('app'),
+    layout: {
+      name: 'grid',
+    },
+    elements: [
+      ...Array(100)
+        .fill(0)
+        .map((_, i) => ({ data: { id: `i${i}`, label: `L${i}` } })),
+    ],
+  });
+  const layers = CytoscapeLayers.layers(cy);
+  // render centered labels on each node
+  layers.renderPerNode(
+    layers.append('html'),
+    (elem) => {
+      elem.style.transform = `${elem.style.transform}scale(${1 / cy.zoom()})`;
+    },
+    {
+      init: (elem, node) => {
+        elem.classList.add('node-label');
+        elem.textContent = node.data('label') || node.id();
+      },
+      transform: 'translate(-50%,-50%)',
+      position: 'center',
+      uniqueElements: true,
+      checkBounds: true,
+    }
+  );
+}
