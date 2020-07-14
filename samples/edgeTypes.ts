f575559e46c2b29eff167f21fe8fbef0ed9c68d0
Samuel Gratzl <sam@sgratzl.com>
2020-07-14 16:49:56 +0200

    fix setup

---
+++ samples/edgeTypes.ts
@@ -0,0 +1,68 @@
+namespace EdgeTypes {
+  declare const cytoscape: typeof import('cytoscape');
+  // declare const CytoscapeLayers: typeof import('../dist');
+
+  export const cy = cytoscape({
+    container: document.getElementById('app'),
+    layout: {
+      name: 'cose',
+    },
+    elements: [
+      ...Array.from('abcdefg').map((d) => ({ data: { id: d } })),
+      ...['ab', 'ac', 'ad', 'ae', 'af', 'ag', 'ga', 'fa', 'ea', 'da', 'ca', 'ba'].map((d) => ({
+        data: { id: d, source: d[0], target: d[1] },
+      })),
+    ],
+    style: [
+      {
+        selector: 'edge',
+        style: {
+          'line-color': 'white',
+          opacity: 0.01,
+        },
+      },
+      {
+        selector: '#ab,#ba',
+        style: {
+          label: 'haystack',
+          'curve-style': 'haystack',
+        },
+      },
+      {
+        selector: '#ac,#ca',
+        style: {
+          label: 'bezier',
+          'curve-style': 'bezier',
+        },
+      },
+      {
+        selector: '#ad,#da',
+        style: {
+          label: 'segments',
+          'curve-style': 'segments',
+        },
+      },
+      {
+        selector: '#ae,#ea',
+        style: {
+          label: 'straight',
+          'curve-style': 'straight',
+        },
+      },
+      {
+        selector: '#af,#fa',
+        style: {
+          label: 'taxi',
+          'curve-style': 'taxi',
+        },
+      },
+      {
+        selector: '#ag,#ga',
+        style: {
+          label: 'unbundled-bezier',
+          'curve-style': 'unbundled-bezier',
+        },
+      },
+    ],
+  });
+}
